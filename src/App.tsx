c481e538afcdc9f8e1af79e7d64f23fd9bb0d912
topette <oscarleonardo.osorio@gmail.com>
2022-09-17 22:41:23 -0500

    Avance en el Weather Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-
+import { WeatherContainer } from "./components/WeatherContainer";
 
 function App() {
-  
-
   return (
-    <div className="App">
-      <h1>Hola Typescript</h1>
+    <div className="flex flex-col justify-center h-screen w-screen items-center shadow-lg">
+      <h1 className="font-bold text-slate-700 text-3xl mb-6">
+        <WeatherContainer />
+      </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/WeatherContainer.tsx
@@ -0,0 +1,11 @@
+export const WeatherContainer = () => {
+  return (
+    <main className="w-96 rounded-3xl bg-gradient-to-b from-gray-800 to-gray-700">
+        <div className="flex w-full items-center flex-col p-8">
+          <h1>Localizacion</h1>
+          <h1>Grados</h1>
+          <h1>Tabla de información</h1>
+        </div>
+    </main>
+  );
+};
